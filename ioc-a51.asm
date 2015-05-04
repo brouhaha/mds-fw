33e9265d1b3e32ad6226b75dbb7e5011812c57a1
Eric Smith <spacewar@gmail.com>
2015-05-04 04:26:10 +0000

    *** empty log message ***

--- ioc-a51.asm
+++ ioc-a51.asm
@@ -11,33 +11,73 @@ romt2c	equ	0002bh
 
 ; entry points in ROM 2
 
-s1003	equ	01003h
+cmd0f	equ	01003h	; cmd 00f - not documented
 s1006	equ	01006h
 s1012	equ	01012h
 
 ; entry points in ROM 3
 
 s1800	equ	01800h
-s1809	equ	01809h
-s180c	equ	0180ch
+keyc	equ	01809h	; cmd 012h - Requests data byte input from the keyboard.
+kstc	equ	0180ch	; cmd 013h - Returns keyboard status byte to master.
 s180f	equ	0180fh
 s1815	equ	01815h
 s181b	equ	0181bh
 
 ; RAM, 4000h..5fffh
 
+	org	04102h
+iopbcnt	ds	1	; count of IOPB bytes received
+
+	org	041f1h
+systatb	ds	1
+
 	org	041f7h
 moshad	ds	1	; RAM shadow of miscout register
-	ds	1
-r41f9	ds	1
+iopb	ds	iopbsiz
 
 	org	05230h
 scrbeg	ds	24*80	; start of screen buffer
 scrend:			; ends at 05a00h (last byte used 059ffh)
 
+; DBB command byte
+cdbbrqi	equ	080h	; host requests that IOC generate command completion interrupt
+
+cpacify	equ	000h	; Reset IOC and its devices.
+cereset	equ	001h	; Reset device-generated error (not used by standard devices).
+csystat	equ	002h	; Returns subsystem status byte to master.
+cdstat	equ	003h	; Returns device status byte to master.
+csrqdak	equ	004h	; Enables input of device int ack mask from master.
+csrqack	equ	005h	; Clears IOC subsystem interrupt request.
+csrq	equ	006h	; Tests ability of IOC to forward an interrupt request to the master.
+cdecho	equ	007h	; Tests ability of IOC to echo data byte sent by master.
+ccsmem	equ	008h	; Requests IOC to checksum on-board ROM. Returns pass/fail.
+ctram	equ	009h	; Requests IOC to test on-board RAM. Returns pass/fail.
+csint	equ	00ah	; Enables specific device interrupt from IOC.
+
+ccrtc	equ	010h	; Requests data byte output to the CRT monitor.
+ccrts	equ	011h	; Returns CRT status byte to master.
+
+ckeyc	equ	012h	; Requests data byte input from the keyboard.
+ckstc	equ	013h	; Returns keyboard status byte to master.
+
+cwpbc	equ	015h	; Enables input from master of IOPB Channel Word byte.
+cwpbcc	equ	016h	; Enables input from master of subsequent IOPB byte.
+cwdbc	equ	017h	; Enables input of diskette write bytes from master.
+crdbc	equ	019h	; Enables output of diskette read bytes to master.
+crrsts	equ	01bh	; Returns diskette result byte to master.
+crdsts	equ	01ch	; Returns diskette device status byte to master.
+
+; SYSTAT bits
+deverr	equ	080h	; device failed to respond to command, use DSTAT cmd for details
+illcmd	equ	040h	; illegal command code received from master
+illdat	equ	020h	; unexpected data from master, or cmd when data expected
+illmsk	equ	010h	; illegal interrupt reset mask from SRQDAK command
+; bits 3..0 reserved
+
 	org	00800h
 
-l0800:	jmp	xl0800
+l0800:	jmp	ereset
 s0803:	jmp	xs0803
 s0806:	jmp	xs0806
 s0809:	jmp	xs0809
@@ -47,21 +87,25 @@ s080c:	jmp	xs080c
 	dw	d099e
 
 
-s0814:	out	iocbusy
-l0816:	in	dbbstat
-	ani	002h
-	jnz	l0816
-	in	dbbstat
-	ani	008h
-	jz	l0828
-	in	dbbin
-	cma
+; get one byte of data from master, return in A
+; BC = address in which to set illegal data transfer flag if command received
+mget1d:	out	iocbusy
+l0816:	in	dbbstat		; has the master written a byte yet?
+	ani	dbbibf
+	jnz	l0816		; no, loop
+	
+	in	dbbstat		; is the byte a command?
+	ani	dbbcd
+	jz	l0828		; yes, error
+
+	in	dbbin		; read the data byte
+	cma			; (hw negative logic)
 	ret
 
 l0828:	ldax	b
 	ori	020h
 	stax	b
-	jmp	xl0800
+	jmp	ereset
 
 
 ; set miscout.unkout4
@@ -140,11 +184,16 @@ s0887:	call	s0880
 	ret
 
 
-s088e:	mvi	b,01ah	; XXX sector count?
-	lxi	h,04232h
-	lxi	d,04266h
-	push	b
+; build map of logical sector number (001h to 01ah) to physical sector number
+; on entry, C is nonzero if sectseq bit was set in IOPB channel word
+s088e:	mvi	b,maxsect
+
+	lxi	h,04232h	; end of table of two-byte logical sect num
+	lxi	d,04266h	; end of table of four-byte phys sect info
+
+	push	b		; save pointers for use in second pass
 	push	d
+
 l0898:	mov	a,m
 	stax	d
 	dcx	h	
@@ -156,12 +205,14 @@ l0898:	mov	a,m
 	dcr	b	
 	jnz	l0898
 
-	pop	h
+	pop	h		; restore pointers for second pass
 	pop	b
-	mov	a,c	
-	ana	a
-	jnz	l08b4
 
+	mov	a,c		; was the sectseq bit set?
+	ana	a
+	jnz	l08b4		; no
+
+; set up remap for format
 l08ab:	mov	m,b	
 	dcx	h
 	dcx	h
@@ -171,7 +222,7 @@ l08ab:	mov	m,b
 	jnz	l08ab
 
 l08b4:	lxi	h,04200h
-	mvi	b,01ah		; XXX sector count?
+	mvi	b,maxsect
 	lda	041fbh
 	mvi	c,000h
 l08be:	mov	m,a
@@ -202,7 +253,8 @@ s08d3:	in	fdcstat
 	ret
 
 
-l08e3:	mov	a,c	
+; cmd 00e - unknown
+cmd0e:	mov	a,c	
 	cpi	04eh
 	lxi	b,00002h
 	lxi	h,05f38h
@@ -242,28 +294,35 @@ l08e3:	mov	a,c
 	jmp	s096a
 
 
-l091e:	lda	05af4h
+; cmd 00d - load memory from master
+cmd0d:	lda	05af4h
 	cpi	024h
-	jnz	l09de
+	jnz	badcmd
 
-	call	s098d
+	call	s098d	; get address and byte count from master
 	jmp	l0932
 
 
+; transfer a number of data blocks from master
+; DE = buffer address
 s092c:	out	iocbusy
 	lxi	b,00d00h
 	xchg
 
-l0932:	mov	a,b	
+; transfer a number of bytes from master
+l0932:	mov	a,b	; if byte count is zero, return
 	ora	c	
 	rz
 
 	dcx	b
 	inr	b	
 	inr	c
-	lxi	d,0080ah
-l093b:	call	s094a
-	dcr	b	
+
+; The first time through this loop may transfer less than 256 bytes,
+; but all subsequent iterations will transfer 256 bytes
+	lxi	d,0080ah	; load dbbstat masks
+l093b:	call	s094a		; transfer C bytes
+	dcr	b		; decrement block count
 	jnz	l093b
 
 	ret
@@ -276,6 +335,11 @@ l0943:	in	dbbin
 	dcr	c	
 	rz	
 
+; transfer 1 to 256 bytes from master
+; HL = address
+; E = dbbstat mask
+; D = required dbbstat bit values
+; C = count (0 will transfer 256 bytes)
 s094a:	in	dbbstat
 	ana	e	
 	cmp	d	
@@ -333,54 +397,57 @@ l0986:	in	dbbstat
 	ret	
 
 
-s098d:	call	s0814
+; receive mem addr, count from master
+; each two-bytes, little endian
+s098d:	call	mget1d
 	mov	l,a	
-	call	s0814
+	call	mget1d
 	mov	h,a	
-	call	s0814
+	call	mget1d
 	mov	c,a	
-	call	s0814
+	call	mget1d
 	mov	b,a	
 	ret
 
 
-d099e:	dw	00000h
-	dw	xl0800
-	dw	l0a57
-	dw	l0a81
-	dw	l0ac7
-	dw	l0b11
-	dw	s09fa
-	dw	l0b21
-	dw	l0b41
-	dw	l0b49
-	dw	l0b54
-	dw	l09de
-	dw	l09de
-	dw	l091e
-	dw	l08e3
-	dw	s1003
-	dw	l0b60
-	dw	l0b73
-	dw	s1809
-	dw	s180c
-	dw	l09de
-	dw	l0b93
-	dw	s0a2d
-	dw	l0ba1
-	dw	l09de
-	dw	l0bb2
-	dw	l09de
-	dw	l0bc5
-	dw	l0bcc
-	dw	l09de
-	dw	l09de
-	dw	l09de
+d099e:	dw	00000h		; 000h cpacify
+	dw	ereset		; 001h cereset
+	dw	systat		; 002h csystat
+	dw	dstat		; 003h cdstat
+	dw	srqdak		; 004h csrqdak
+	dw	srqack		; 005h csrqack
+	dw	srq		; 006h csrq
+	dw	decho		; 007h cdecho
+	dw	csmem		; 008h ccsmem
+	dw	tram		; 009h ctram
+	dw	sint		; 00ah csint
+	dw	badcmd		; 00bh
+	dw	badcmd		; 00ch
+	dw	cmd0d		; 00dh ?
+	dw	cmd0e		; 00eh ?
+	dw	cmd0f		; 00fh ?
+	dw	crtc		; 010h ccrtc
+	dw	crts		; 011h ccrts
+	dw	keyc		; 012h ckeyc
+	dw	kstc		; 013h ckstc
+	dw	badcmd		; 014h
+	dw	wpbc		; 015h cwpbc
+	dw	wpbcc		; 016h cwpbcc
+	dw	wdbc		; 017h cwdbc
+	dw	badcmd		; 018h
+	dw	rdbc		; 019h crdbc
+	dw	badcmd		; 01ah
+	dw	rrsts		; 01bh crrsts
+	dw	rdsts		; 01ch crdsts
+	dw	badcmd		; 01dh
+	dw	badcmd		; 01eh
+	dw	badcmd		; 01fh
 
 
-l09de:	lda	041f1h
-	ori	040h
-	sta 	041f1h
+; invalid command
+badcmd:	lda	systatb
+	ori	illcmd
+	sta 	systatb
 	ret
 
 
@@ -396,8 +463,9 @@ s09e7:	in	kbdstat
 l09f9:	ret
 
 
+; cmd 006h - Tests ability of IOC to forward an interrupt request to the master.
 ; pulse miscout.unkout4 low very briefly
-s09fa:	lda	moshad
+srq:	lda	moshad
 	ani	0fdh
 	out	miscout
 	lda	moshad
@@ -410,7 +478,7 @@ xs080f:	lda	041efh
 	lxi	h,041f2h
 	ora	m
 	mov	m,a	
-	call	s09fa
+	call	srq
 	lxi	h,041eeh
 	mvi	m,0ffh
 	ret
@@ -427,19 +495,21 @@ s0a1a:	lda	041edh
 l0a2c:	ret
 
 
-s0a2d:	mvi	a,005h
-	lxi	h,04102h
+; cmd 016h - Enable input from master of subsequent IOPB byte.
+; (note that wpbc actually calls this for the Channel Word byte.)
+wpbcc:	mvi	a,005h		; check if full IOPB already received
+	lxi	h,iopbcnt
 	cmp	m
-	jc	l0a56
+	jc	l0a56		; yes, error
 
 	lxi	b,041ffh
-	call	s0814
-	lhld	04102h
+	call	mget1d
+	lhld	iopbcnt
 	mvi	h,000h
-	lxi	b,041f8h
+	lxi	b,iopb
 	dad	b
 	mov	m,a	
-	lxi	h,04102h
+	lxi	h,iopbcnt
 	inr	m
 	mov	a,m
 	cpi	005h
@@ -450,7 +520,13 @@ l0a53:	call	s0a1a
 l0a56:	ret
 
 
-l0a57:	lda	041f4h
+; cmd 002h - Returns subsystem status byte to master
+; bit 7 - device error
+; bit 6 - illegal command
+; bit 5 - illegal data transfer
+; bit 4 - illegal interrupt mask
+; bits 3..0 - reserved
+systat:	lda	041f4h
 	lxi	h,041f3h
 	ana	m
 	lxi	h,041ffh
@@ -459,21 +535,31 @@ l0a57:	lda	041f4h
 	cpi	000h
 	jz	l0a71
 
-	lda	041f1h
-	ori	080h
-	sta	041f1h
+	lda	systatb
+	ori	deverr
+	sta	systatb
 
-l0a71:	lda	041f1h
-	cma
+l0a71:	lda	systatb		; output systat byte
+	cma			; (hw negative logic)
 	out	dbbout
-	lxi	h,041f1h
+
+	lxi	h,systatb	; clear systat byte
 	mvi	m,000h
+
 	mvi	a,000h
 	out	clrcd
 	ret
 
 
-l0a81:	lda	041f3h
+; cmd 003h - returns device status byte to master
+; bit 7 - diskette error
+; bit 6 - keyboard error
+; bit 5 - CRT error
+; bits 4..3 reserved
+; bit 2 - diskette interrupt
+; bit 1 - keyboard interrupt
+; bit 0 - CRT interrupt
+dstat:	lda	041f3h
 	ani	0f0h
 	cpi	000h
 	jz	l0a93
@@ -507,8 +593,13 @@ l0ab7:	lda	041f2h
 	ret
 
 
-l0ac7:	lxi	b,041f1h
-	call	s0814
+; cmd 004h - Enables input of device interrupt acknowledge mask from master.
+; bits 7..3 reserved
+; bit 2 - diskette interrupt reset
+; bit 1 - keyboard interrupt reset
+; bit 0 - CRT interrupt reset
+srqdak:	lxi	b,systatb
+	call	mget1d
 	sta	04101h
 	lda	04101h
 	ani	007h
@@ -521,9 +612,9 @@ l0ac7:	lxi	b,041f1h
 	cpi	000h
 	jz	l0aed
 
-	lda	041f1h
-	ori	010h
-	sta	041f1h
+	lda	systatb
+	ori	illmsk
+	sta	systatb
 	jmp	l0b10
 
 
@@ -549,7 +640,8 @@ l0aed:	lda	04101h
 l0b10:	ret
 
 
-l0b11:	lxi	h,041efh
+; cmd 005h - Clears IOC subsystem interrupt request
+srqack:	lxi	h,041efh
 	mvi	m,000h
 	lxi	h,041f2h
 	mvi	m,000h
@@ -558,8 +650,9 @@ l0b11:	lxi	h,041efh
 	ret
 
 
-l0b21:	lxi	b,041f1h
-	call	s0814
+; cmd 007h - Tests ability of IOC to echo data byte sent by master
+decho:	lxi	b,systatb
+	call	mget1d
 	sta	04103h
 	lda	041edh
 	cpi	027h
@@ -574,26 +667,36 @@ l0b37:	lda	04103h
 	ret
 
 
-l0b41:	mvi	c,000h
+; cmd 008h - Requests IOC to checksum on-board ROM. Returns pass/fail.
+csmem:	mvi	c,000h
 	call	romt2c	; same as romt2k, but with arg in C
 	out	dbbout
 	ret
 
 
-l0b49:	lxi	d,04200h
+; cmd 009h - Requests IOC to test on-board RAM. Returns pass/fail.
+tram:	lxi	d,04200h
 	mvi	c,001h
 	call	s000b
 	out	dbbout
 	ret
 
-l0b54:	lxi	b,041f1h
-	call	s0814
+
+; cmd 00ah - Enables specified device interrupt from IOC.
+; bits 7..3 reserved
+; bit 2 - diskette interrupt enable
+; bit 1 - keyboard interrupt enable
+; bit 0 - CRT interrupt enable
+sint:	lxi	b,systatb
+	call	mget1d
 	ani	007h
 	sta	041f0h
 	ret
 
-l0b60:	lxi	b,041f3h
-	call	s0814
+
+; cmd 010h - Requests data byte output to the CRT monitor.
+crtc:	lxi	b,041f3h
+	call	mget1d
 	mov	c,a	
 	call	05ff5h
 	lda	041efh
@@ -602,7 +705,13 @@ l0b60:	lxi	b,041f3h
 	ret
 
 
-l0b73:	lda	041edh
+; cmd 011h - Returns CRT status byte to master.
+; bit 7 - reserved
+; bit 6 - illegal status
+; bit 5 - illegal data
+; bits 4..1 - reserved
+; bit 0 - CRT present
+crts:	lda	041edh
 	ani	080h
 	cpi	000h
 	jz	l0b85
@@ -619,24 +728,29 @@ l0b85:	lda	041f3h
 	ret
 
 
-l0b93:	lxi	h,04102h
+; cmd 015h - Enables input from master of IOPB Channel Word byte.
+wpbc:	lxi	h,iopbcnt
 	mvi	m,000h
 	mvi	c,0f7h
 	call	s083f
-	call	s0a2d
+	call	wpbcc
 	ret
 
 
-l0ba1:	lxi	b,041ffh
-	call	s0814
-	mov	c,a	
-	lxi	d,04200h
-	call	s092c
+; cmd 017h - Enables input of diskette write bytes from master.
+; The first byte transferred from the master is the number of 128-byte
+; records to follow.
+wdbc:	lxi	b,041ffh
+	call	mget1d
+	mov	c,a		; block count
+	lxi	d,04200h	; buffer address
+	call	s092c		; transfer data blocks from master
 	call	s0a1a
 	ret
 
 
-l0bb2:	mvi	c,008h
+; cmd 019h - Enables input of diskette read bytes to master.
+rdbc:	mvi	c,008h
 	call	s0834
 	lhld	041fah
 	mov	c,l	
@@ -646,13 +760,15 @@ l0bb2:	mvi	c,008h
 	ret
 
 
-l0bc5:	call	xs0809
+; cmd 01bh - Returns disk result byte to master.
+rrsts:	call	xs0809
 	cma
 	out	dbbout
 	ret
 
 
-l0bcc:	call	xs0806
+; cmd 01ch - Returns diskette device status byte to master.
+rdsts:	call	xs0806
 	lda	041f0h
 	ani	004h
 	cpi	000h
@@ -767,7 +883,8 @@ l0cae:	mvi	a,000h
 	ret	
 
 
-xl0800:	lxi	h,04080h
+; cmd 001h - Resets device-generated error (not used by standard devices).
+ereset:	lxi	h,04080h
 	sphl
 	call	s0c02
 	call	s181b
@@ -867,10 +984,11 @@ s0d50:	lhld	0410bh
 	ret
 
 
-s0d7b:	lda	041f8h
-	ani	040h
+s0d7b:	lda	iopb+iopbocw	; get channel word
+	ani	sectseq
 	mov	c,a	
 	call	s088e
+
 	mvi	e,067h
 	lxi	b,04200h
 	call	s0848
@@ -923,7 +1041,7 @@ l0de8:	ret
 xs0803:	call	s083d
 	lxi	h,041f5h
 	mvi	m,080h
-	lda	r41f9
+	lda	iopb+iopboin	; get diskette instruction byte
 	ani	007h
 	mov	c,a	
 	mvi	b,000h
